b8796cad9c287c6b5f14db96674d82faf8c185e6
drwolf1999 <drwolf1888@gmail.com>
2020-08-24 17:38:30 +0900

    conflict fix

--- caps-problem-solving-frontend/src/constants/RestAPI.js
+++ caps-problem-solving-frontend/src/constants/RestAPI.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 //const SERVER_DOMAIN = 'http://192.168.0.12:3000/';
 const SERVER_DOMAIN = 'http://192.168.0.40:3000/';
-=======
-const SERVER_DOMAIN = 'http://192.168.0.25:3000/';
->>>>>>> f7ce50c1c0d0c0c006f17b10832834a966728ab7
 
 export default {
     SERVER_DOMAIN: SERVER_DOMAIN,
